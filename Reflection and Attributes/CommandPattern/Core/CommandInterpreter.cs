39057a775b74b81cdadc8efa7ad3fe135404d42d
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-08 12:20:54 +0200

    100/100

---
+++ Reflection and Attributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+
+        public CommandInterpreter()
+        {
+            
+        }
+
+        public string Read(string args)
+        {
+            var commandTokens = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var commandName = commandTokens[0]+COMMAND_POSTFIX;
+            var commandArgs = commandTokens.Skip(1).ToArray();
+
+            var assembly = Assembly.GetCallingAssembly();
+            var commandType = assembly.GetTypes().FirstOrDefault(t => t.Name.ToLower() == commandName.ToLower());
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            var commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+            var result = commandInstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ Reflection and Attributes/CommandPattern/Core/Engine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var args = Console.ReadLine();
+
+                try
+                {
+                    var result = this.commandInterpreter.Read(args);
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
