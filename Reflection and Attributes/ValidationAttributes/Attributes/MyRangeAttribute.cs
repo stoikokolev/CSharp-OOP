8487975c8c64a15689c1aace28898129fc25f36a
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-10 18:47:58 +0200

    100/100

---
+++ Reflection and Attributes/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.ValidateRange(minValue,maxValue);
+            this.maxValue = maxValue;
+            this.minValue = minValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if (obj is int value)
+            {
+                return value >= this.minValue && value <= this.maxValue;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot validate give data type!");
+            }
+        }
+
+        private void ValidateRange(int minValuePar, int maxValuePar)
+        {
+            if (minValuePar > maxValuePar)
+            {
+                throw new ArgumentException("Invalid range!");
+            }
+        }
+    }
+}
---
+++ Reflection and Attributes/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Check all object's properties for custom attributes and if all custom attributes are valid, then the whole object is valid
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool IsValid(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var objType = obj.GetType();
+            var properties = objType.GetProperties();
+
+            // If all properties are valid with their custom attributes -> Object is Valid
+            foreach (PropertyInfo property in properties)
+            {
+                var attributes = property.GetCustomAttributes()
+                    .Where(ca => ca is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                if (attributes.Any(attribute => !attribute.IsValid(property.GetValue(obj))))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
